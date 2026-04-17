9efe14830d115d42a02b3c07fdb65444aa11f6b1
faress1212 <fares.shawat2@gmail.com>
2026-04-17 20:51:11 +0200

    Server Client

---
+++ server.py
@@ -0,0 +1,29 @@
+import socket
+import threading
+
+clients = []
+
+def handle(conn):
+    while True:
+        try:
+            msg = conn.recv(1024)
+            if not msg:
+                break
+            for c in clients:
+                if c != conn:
+                    c.send(msg)
+        except:
+            break
+    clients.remove(conn)
+    conn.close()
+
+server = socket.socket()
+server.bind(('0.0.0.0', 50050))
+server.listen(5)
+print("Server running...")
+
+while True:
+    conn, addr = server.accept()
+    clients.append(conn)
+    print(f"Connected: {addr}")
+    threading.Thread(target=handle, args=(conn,), daemon=True).start()
